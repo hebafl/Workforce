242f552c0474fde3ffdf098aa38df37f9af65534
hebafl <73886364+hebafl@users.noreply.github.com>
2022-09-28 16:06:40 +0300

    Create a test python file

---
+++ test.py
@@ -0,0 +1,7 @@
+animals = ["monkey", "lion", "elephant", "rhino", "jackal"]
+
+for animal in animals:
+    if animal == "elephant":
+        print("elephant found. Terminating the loop")
+        break
+    print(animal)
